3053122844b12e4dd4f9432d1b21edf79eba3cc2
Gaf Dev <azizjonbloxfruit@gmail.com>
2025-08-12 13:15:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let mevalar = ["Banan", "Olma", "Nok", "Olxori"];
+console.log(mevalar);
+
+alert(`Massivdagi mevalar soni: ${mevalar.length}`);
+
+confirm("Siz 1 ta mevaning oxirgisini kesib tashlamoqchimisiz?") 
+ mevalar.pop();
+
+alert(`Endi massivdagi mevalar soni: ${mevalar.length}`);
